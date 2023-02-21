f75168897cdb3a3d37b4a30f58b1dbfa506f17cf
faizabdullah0134 <faizahf0134@gmail.com>
2023-02-21 15:37:17 +0700

    Praktikum1

---
+++ Praktikum1/index.php
@@ -0,0 +1,51 @@
+<?php
+
+// in sebuah komentar
+/* ini sebuah komentar */
+
+// echo "Hello World <br>";
+// print_r("Nama saya Faiz <br>");
+// var_dump("kuliah di STT Terpadu Nurul FIkri");
+
+// // Membuat variabel user
+// $nama = "Faiz Abdullah Hanif Firmansyah";
+// $umur = 18;
+// $berat = 50.5;
+// $mahasiswa = true;
+
+// echo"Nama saya adalaah $nama <br>";
+// echo"Umur saya adaalah $umur tahun <b>";
+// echo"berat badan $berat kg <br>";
+
+// // Membuat variabel sistem
+// echo "Dokumen root" . $_SERVER["DOCUMENT_ROOT"];
+// echo "Nama File" . $_SERVER['PHP_SELF'];
+
+//Membuat variabel konstanta
+// define("PHI", 3.14);
+// $jari = 8;
+// $luas = PHI * $jari * $jari;
+// $keliling = 2 * PHI * $jari;
+
+// echo "Luas lingkaran dengan jari-jari = $jari = $luas <br>";
+// echo "Keliling lingkaran adalah $keliling";
+
+
+// Membuat Array
+$programs = ["php", "Javascript", "HTML", "CSS"];
+// echo $programs[0];
+// echo count($programs);
+// $programs[] = "Mysql";
+// echo count($programs);
+
+// unset($programs[0]);
+// echo "<br>" . $programs[0];
+
+// echo "<ol>";
+// foreach($programs as $program){
+//     echo "<li> $program </li>";
+// }
+// echo "</ol>";
+
+
+?>
